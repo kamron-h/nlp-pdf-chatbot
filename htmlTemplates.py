78ce53c22236334eaf67e28487d442c0ec268e91
Kamron <kamronthopkins@gmail.com>
2024-03-01 01:16:25 -0500

    Update avatar image URLs in html templates
    The URLs for the avatar images in both the bot and user templates have been updated. These changes took place in `htmlTemplates.py` and represent new source locations for the respective images.

--- htmlTemplates.py
+++ htmlTemplates.py
@@ -28,7 +28,7 @@ css = '''
 bot_template = '''
 <div class="chat-message bot">
     <div class="avatar">
-        <img src="https://i.ibb.co/cN0nmSj/Screenshot-2023-05-28-at-02-37-21.png" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Favp%2Fimages%2Fa%2Fa8%2FDavidcovenant.jpg%2Frevision%2Flatest%3Fcb%3D20170527212857&f=1&nofb=1&ipt=04f4281e03062cd3865963145833e3ec1bd1f961404edf62a0dae8df52631a1a&ipo=images" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
     </div>
     <div class="message">{{MSG}}</div>
 </div>
@@ -37,7 +37,8 @@ bot_template = '''
 user_template = '''
 <div class="chat-message user">
     <div class="avatar">
-        <img src="https://i.ibb.co/rdZC7LZ/Photo-logo-1.png">
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdenovosoftware.com%2Fwp-content%2Fuploads%2Fface-placeholder.png&f=1&nofb=1&ipt=19dc5fbaafb063c8f717bbd29c89904adf8f3ce5a2cd74fa4971c860cc737628&ipo=images">
+        <h6
     </div>    
     <div class="message">{{MSG}}</div>
 </div>
